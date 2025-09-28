77c83fb6aee7c162176857af5b29e2f60fdcdf6a
George <xxxvodo4kaxxx@gmail.com>
2025-09-28 20:46:56 +0300

    books/condition is done

---
+++ labs/lab_2/BookWarehouse/include/books/BookCondition.hpp
@@ -0,0 +1,24 @@
+#include <string>
+
+class BookCondition {
+public:
+    enum class Condition {
+        NEW,        // новая
+        LIKE_NEW,   // как новая
+        VERY_GOOD,  // очень хорошее
+        GOOD,       // хорошее
+        FAIR,       // удовлетворительное  
+        POOR        // плохое
+    };
+private:
+    Condition condition;
+public:
+    explicit BookCondition(Condition condition) noexcept;
+    Condition getCondition() const noexcept;
+    std::string toString() const noexcept;
+    bool isNew() const noexcept;
+    bool isUsed() const noexcept;
+    bool needsReplacement() const noexcept; // POOR - требует замены
+    bool operator==(const BookCondition& other) const noexcept;
+    bool operator!=(const BookCondition& other) const noexcept;
+};
